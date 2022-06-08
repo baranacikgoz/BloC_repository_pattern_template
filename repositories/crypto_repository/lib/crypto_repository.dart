068f9b256e5bae9ca02ea7e49ce892d223be2240
baranacikgoz <baran-acikgoz@outlook.com>
2022-06-08 16:26:52 +0300

    created crypto_repository

---
+++ repositories/crypto_repository/lib/crypto_repository.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library crypto_repository;
+
+export 'src/crypto_repository_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
