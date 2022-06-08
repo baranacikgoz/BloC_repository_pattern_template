540c51ea363d2736f9921d132a7e78ebbdfed8ce
baranacikgoz <baran-acikgoz@outlook.com>
2022-06-08 15:55:58 +0300

    removed has_wifi_rtt package

--- packages/has_wifi_rtt/example/README.md
+++
@@ -1,3 +0,0 @@
-# has_wifi_rtt_example
-
-Demonstrates how to use the has_wifi_rtt plugin.
--- packages/has_wifi_rtt/example/android/app/src/main/kotlin/com/example/has_wifi_rtt_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.has_wifi_rtt_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
